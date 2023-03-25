91d9f32d914a60d80fe429b903d3472d9173072c
Udara J(SiniSTeR) <67888873+udaraJ08@users.noreply.github.com>
2023-03-25 19:10:13 +0530

    hard ware managing done

--- Controllers/ManagerController.cs
+++ Controllers/ManagerController.cs
@@ -33,13 +33,109 @@ namespace e_comm.Controllers
             return View(list);
         }
 
+        public ActionResult ManagerRam()
+        {
+            Database db = new Database();
+            db.Open();
+            SqlDataReader reader = db.ExecuteQuery("SELECT * FROM ram");
+            List<RamModal> list = new List<RamModal>();
+            while (reader.Read())
+            {
+                RamModal category = new RamModal();
+                category.Id = (int)reader["id"];
+                category.Title = (string)reader["title"];
+                category.Price = (decimal)reader["price"];
+                category.Size = (int)reader["size"];
+                list.Add(category);
+            }
+            db.Close();
+            return View(list);
+        }
+
+        public ActionResult ManagerHard()
+        {
+            Database db = new Database();
+            db.Open();
+            SqlDataReader reader = db.ExecuteQuery("SELECT * FROM hard");
+            List<HardModal> list = new List<HardModal>();
+            while (reader.Read())
+            {
+                HardModal category = new HardModal();
+                category.Id = (int)reader["id"];
+                category.Title = (string)reader["title"];
+                category.Price = (decimal)reader["price"];
+                category.Storage = (int)reader["storage"];
+                list.Add(category);
+            }
+            db.Close();
+            return View(list);
+        }
+
+        public ActionResult ManagerProcessor()
+        {
+            Database db = new Database();
+            db.Open();
+            SqlDataReader reader = db.ExecuteQuery("SELECT * FROM processer");
+            List<ProcessorModal> list = new List<ProcessorModal>();
+            while (reader.Read())
+            {
+                ProcessorModal category = new ProcessorModal();
+                category.Id = (int)reader["id"];
+                category.Title = (string)reader["title"];
+                category.Price = (decimal)reader["price"];
+                category.Clock_speed = (string)reader["clock_speed"];
+                list.Add(category);
+            }
+            db.Close();
+            return View(list);
+        }
+
         [HttpPost("/vga/create")]
-        public HttpStatusCode Order(string title, decimal price, int size)
+        public HttpStatusCode VgaCreate(string title, decimal price, int size)
         {
             Database db = new Database();
             db.Open();
 
             string query = "insert into vga (title, price, size) values ('" + title + "', " + price + ", " + size + ")";
+
+            db.ExecuteNonQuery(query);
+
+            return HttpStatusCode.Created;
+        }
+
+        [HttpPost("/processor/create")]
+        public HttpStatusCode ProcessorCreate(string title, decimal price, string clock_speed)
+        {
+            Database db = new Database();
+            db.Open();
+
+            string query = "insert into processer (title, price, clock_speed) values ('" + title + "', " + price + ", '" + clock_speed + "')";
+
+            db.ExecuteNonQuery(query);
+
+            return HttpStatusCode.Created;
+        }
+
+        [HttpPost("/ram/create")]
+        public HttpStatusCode RamCreate(string title, decimal price, int size)
+        {
+            Database db = new Database();
+            db.Open();
+
+            string query = "insert into ram (title, price, size) values ('" + title + "', " + price + ", " + size + ")";
+
+            db.ExecuteNonQuery(query);
+
+            return HttpStatusCode.Created;
+        }
+
+        [HttpPost("/hard/create")]
+        public HttpStatusCode HardCreate(string title, decimal price, int storage)
+        {
+            Database db = new Database();
+            db.Open();
+
+            string query = "insert into hard (title, price, storage) values ('" + title + "', " + price + ", " + storage + ")";
 
             db.ExecuteNonQuery(query);
 
--- wwwroot/js/ManageHardWare.js
+++ wwwroot/js/ManageHardWare.js
@@ -28,3 +28,81 @@ const addNewVga = () => {
         error: function (responce) { console.error(responce.data) }
     });
 }
+
+const addNewRam= () => {
+
+    const title = $('#ramTitle').val()
+    const size = $('#ramSize').val()
+    const price = $('#ramPrice').val()
+
+
+    $.ajax({
+        url: '/ram/create',
+        type: 'POST',
+        data: { title, price, size },
+        success: function (response) {
+
+            Swal.fire({
+                title: 'Done',
+                text: 'New Ram added successfully !',
+                icon: 'success',
+                confirmButtonText: 'close'
+            })
+
+            window.location.reload()
+        },
+        error: function (responce) { console.error(responce.data) }
+    });
+}
+
+const addNewProcessor= () => {
+
+    const title = $('#processorTitle').val()
+    const Clock_speed = $('#processorClockSpeed').val()
+    const price = $('#processorPrice').val()
+
+
+    $.ajax({
+        url: '/processor/create',
+        type: 'POST',
+        data: { title, price, Clock_speed },
+        success: function (response) {
+
+            Swal.fire({
+                title: 'Done',
+                text: 'New Processor added successfully !',
+                icon: 'success',
+                confirmButtonText: 'close'
+            })
+
+            window.location.reload()
+        },
+        error: function (responce) { console.error(responce.data) }
+    });
+}
+
+const addNewHard= () => {
+
+    const title = $('#hardTitle').val()
+    const storage = $('#hardStorage').val()
+    const price = $('#hardPrice').val()
+
+
+    $.ajax({
+        url: '/hard/create',
+        type: 'POST',
+        data: { title, price, storage },
+        success: function (response) {
+
+            Swal.fire({
+                title: 'Done',
+                text: 'New Hard added successfully !',
+                icon: 'success',
+                confirmButtonText: 'close'
+            })
+
+            window.location.reload()
+        },
+        error: function (responce) { console.error(responce.data) }
+    });
+}
